98ce798cfb5303ed13a2455dfcdf8124d3da34d3
Spectre <antoine@doussaud.org>
2018-06-20 00:36:51 +0200

    color

--- test_udp.lua
+++ test_udp.lua
@@ -10,11 +10,15 @@ local port		= 12345
 local LED_RAW	= 0
 
 while true do
-
-	for i=0,30 do
+	local t = 100
+	for i=0,t do
 		local to_send = struct.pack('Bhh', LED_RAW, 0, 30)
 		for j=0,30 do
-			to_send = to_send .. struct.pack('BBB', 0, 0, (math.sin((j-i/60)*2*math.pi) + 1) * 100 + 50)
+			to_send = to_send .. struct.pack('BBB',
+				((math.sin(((j+i)%t/t) * 2 * math.pi + math.pi) + 1) / 2) * 50,
+				((math.sin(((j+i)%t/t) * 2 * math.pi) + 1) / 2) * 10,
+				((math.sin(((j+i)%t/t) * 2 * math.pi) + 1) / 2) * 255
+			)
 		end
 		assert(udp:sendto(to_send, ip, port))
 		socket.sleep(1/20)
