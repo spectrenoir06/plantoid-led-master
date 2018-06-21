70e77cb504091f1e4352ee020407dbaaf788494c
Spectre <antoine@doussaud.org>
2018-06-21 21:17:51 +0200

    log ok

--- test_udp.lua
+++ test_udp.lua
@@ -1,131 +1,20 @@
 local socket = require("socket")
 local inspect = require("inspect")
-
+local osc = require "osc"
 require "struct"
 
 local LED_IP        = "192.168.12.50"
 local LED_PORT      = 12345
+
+local MUSIC_IP        = "127.0.0.1"
+local MUSIC_PORT      = 54321
+
 local SERVER_PORT   = 8000
 
 local TYPE_LED_RAW  = 0
 local TYPE_GET_INFO = 1
 
 local LED_FRAMERATE = 20
-
-
-function decode_frac(bin)
-	local frac = 0
-	for i=#bin,1 do
-		frac = (frac + string.sub(bin, i-1, i)) / 2
-	end
-	return frac
-end
-
-function decode_float(bin)
-	local res = struct.unpack(">f", bin)
-	return res
-end
-
-function decode_int(bin)
-	local res = struct.unpack(">I", bin)
-	return res
-end
-
-
-function next_string(astring)
-	-- this is a workaraound because the lua pttern matching is
-	-- not as powerful as pcre and I did not want to include another
-	-- dependecy to an external re lib
-	local pos = 0
-	local num_nzero = 0
-	local num_zero = 0
-	local result = ""
-	if astring == nil then
-		-- ensure that string is not empty
-		print("error: string is empty - probably malformated message")
-	end
-	-- we match every character with the help of gmatch
-	for m in string.gmatch(astring, ".") do
-		pos = pos + 1
-		-- and then check if it is correctly padded with '\0's
-		if m ~= '\0' and num_zero == 0 then
-			num_nzero = (num_nzero + 1) % 4
-			result = result .. m
-		elseif num_zero ~= 0 and (num_zero + num_nzero) % 4 == 0 then
-			return result, pos
-		elseif m == '\0' then
-			num_zero = num_zero + 1
-			result = result .. m
-		else
-			return nil
-		end
-	end
-end
-
-function collect_decoding_from_message(t, data, message)
-	table.insert(message, t)
-	if t == 'i' then
-		table.insert(message, decode_int(data))
-		return string.sub(data, 5)
-	elseif t == 'f' then
-		table.insert(message, decode_float(data))
-		return string.sub(data, 5)
-	elseif t == 's' then
-		local match, last = next_string(data)
-		table.insert(message, match)
-		return string.sub(data, last)
-	elseif t == 'b' then
-		local length = decode_int(data)
-		table.insert(message, string.sub(data, 4, length))
-		return string.sub(data, 4 + length + 1)
-	end
-end
-
-
-function get_addr_from_data(data)
-	local addr_raw_string,last = next_string(data)
-	local result = ""
-	if addr_raw_string == nil then
-		-- if we could not find an addr something went wrong
-		print("error: could not extract address from OSC message")
-	end
-	-- delete possible trailing zeros
-	for t in string.gmatch(addr_raw_string, "[^%z]") do
-		result = result .. t
-	end
-	return result, string.sub(data, last)
-end
-
-function get_types_from_data(data)
-	local typestring, last = next_string(data)
-	local result = {}
-	if typestring == nil then
-		return {}
-	end
-	-- split typestring into an iterable table
-	for t in string.gmatch(typestring, "[^,%z]") do
-		table.insert(result, t)
-	end
-	return result, string.sub(data, last)
-end
-
-
-
-function decode_message(data)
-	local types, addr, tmp_data = nil
-	local message = {}
-	addr, tmp_data = get_addr_from_data(data)
-	types, tmp_data = get_types_from_data(tmp_data)
-	-- ensure that we at least found something
-	if addr == nil or types == nil then
-		return nil
-	end
-	for _,t in ipairs(types) do
-		tmp_data = collect_decoding_from_message(t, tmp_data, message)
-	end
-	return message
-end
-
 
 local udp = assert(socket.udp())
 udp:setsockname("*", SERVER_PORT)
@@ -151,24 +40,74 @@ local timer_led = 0
 
 sensors = {}
 
+local time_start_epoch = os.time(os.date("!*t"))
+local time_start_cpu   = socket.gettime()
+
+local replay = false
+local replay_index = 1
+local replay_dump = {}
+
+function load_dump(name)
+	local file = io.open(name, "r")
+	local lines = {}
+
+	local timestamp = file:read()
+
+	while true do
+		line = file:read()
+		if line == nil then break end
+		time, address, data = line:match("([^,]+);([^,]+);([^,]+)")
+		table.insert(lines, {tonumber(time), address, tonumber(data)})
+	end
+	file:close()
+	return lines
+end
+
+if arg[1] == "replay" then
+	print("Start replay:", arg[2])
+	replay_dump = load_dump(arg[2])
+	replay = true
+	replay_len = #replay_dump
+else
+	replay = false
+	print("start dump:","log.dump")
+	file = io.open("log.dump", "w")
+	file:write(time_start_epoch .. "\n")
+end
+
 while true do
 	dt = socket.gettime() - time
 	time = socket.gettime()
 
 	timer_led = timer_led + dt
-	if timer_led > (1/LED_FRAMERATE) then
+	if timer_led > (1 / LED_FRAMERATE) then
 		send_led(counter/100)
 		counter = counter + 1
 		timer_led = 0
 	end
 
-	data, ip, port = udp:receivefrom() -- receive from led, adc and super collider
-	if data then
-		print("Received: ", ip, port, #data)
-		local addr = get_addr_from_data(data)
-		local value = decode_message(data)
-		sensors[addr] = value[2];
-		print(inspect(sensors))
+	if replay then
+		if replay_index < replay_len then
+			if replay_dump[replay_index][1] <= socket.gettime() - time_start_cpu then
+				local to_send = { replay_dump[replay_index][2], { 'f', replay_dump[replay_index][3]}}
+				print(inspect(to_send))
+				assert(udp:sendto(osc.encode(to_send), MUSIC_IP, MUSIC_PORT))
+				replay_index = replay_index + 1
+			end
+		else
+			print("Replay finish")
+			break
+		end
+	else
+		local data, ip, port = udp:receivefrom() -- receive from led, adc and super collider
+		if data then
+			-- print("Received: ", ip, port, #data)
+			local addr = osc.get_addr_from_data(data)
+			local value = osc.decode(data)
+			sensors[addr] = value[2]
+			print(socket.gettime() - time_start_cpu, addr, value[2])
+			file:write((socket.gettime() - time_start_cpu)..";"..addr..";"..value[2].."\n")
+		end
 	end
-	socket.sleep(0.01)
+	socket.sleep(0.0001)
 end
