67428429428858a27856350dc57661694362de7b
Spectre <antoine@doussaud.org>
2018-06-23 00:14:07 +0200

    menage

--- led_master.lua
+++ led_master.lua
@@ -5,36 +5,22 @@ local socket  = require("socket")
 local inspect = require("lib.inspect")
 local struct  = require("lib.struct")
 
-local LED_IP        = "192.168.12.50"
-local LED_PORT      = 12345
+local LED_IP        = "192.168.11.50"        -- ip to connect to led driver
+local LED_PORT      = 12345                  -- port to connect to led driver
 
-local MUSIC_IP       = "127.0.0.1"
-local MUSIC_PORT     = 54321
+local CLIENT_MUSIC_IP       = "127.0.0.1"    -- ip to connect to super collider
+local CLIENT_MUSIC_PORT     = 54321          -- port to connect to super collider
 
-local SERVER_PORT   = 8000
+local SERVER_SENSOR_PORT = 8000              -- port to listen to sensors OSC data
+local SERVER_MUSIC_PORT  = 8001              -- port to listen to super collider OSC data
+local SERVER_DATA_PORT   = 8002              -- port to listen to other data ( led info, cmd, ...)
 
 local TYPE_LED_RAW  = 0
-local TYPE_GET_INFO = 1
+local TYPE_LED_RAW_RGBW = 1
+local TYPE_GET_INFO = 2
 
 local LED_FRAMERATE = 20
 
-local udp = assert(socket.udp())
-udp:setsockname("*", SERVER_PORT)
-udp:settimeout(0)
-
-function send_led(c)
-	local to_send = struct.pack('Bhh', TYPE_LED_RAW, 0, 30)
-	for j=0,30 do
-		to_send = to_send .. struct.pack('BBB',
-			((math.sin((j+c) * 2 * math.pi + math.pi) + 1) / 2) * 20,
-			((math.sin((j+c) * 2 * math.pi) + 1) / 2) * 20,
-			((math.sin((j+c) * 2 * math.pi) + 1) / 2) * 100 + 40
-		)
-	end
-	assert(udp:sendto(to_send, LED_IP, LED_PORT))
-end
-
-local counter = 0
 local dt = 0.000001
 local time = socket.gettime()
 
@@ -42,12 +28,31 @@ local timer_led = 0
 
 sensors = {}
 
-local time_start_epoch = os.time(os.date("!*t"))
-local time_start_cpu   = socket.gettime()
-
 local replay = false
 local replay_index = 1
 local replay_dump = {}
+
+local time_start_epoch = os.time(os.date("!*t"))
+local time_start_cpu   = socket.gettime()
+
+local counter = 0
+
+local udp = assert(socket.udp())
+udp:setsockname("*", SERVER_SENSOR_PORT)
+udp:settimeout(0)
+
+
+
+
+
+function send_led(c)
+	local to_send = struct.pack('Bhh', TYPE_LED_RAW_RGBW, 0, 93)
+	for j=0,93 do
+		local r, g, b = color_wheel(c*5)
+		to_send = to_send .. struct.pack('BBB', r, g, b)
+	end
+	assert(udp:sendto(to_send, LED_IP, LED_PORT))
+end
 
 function load_dump(name)
 	local file = io.open(name, "r")
@@ -65,6 +70,21 @@ function load_dump(name)
 	return lines
 end
 
+function color_wheel(WheelPos)
+	WheelPos = WheelPos % 255
+	WheelPos = 255 - WheelPos
+	if (WheelPos < 85) then
+		return 255 - WheelPos * 3, 0, WheelPos * 3
+	elseif (WheelPos < 170) then
+		WheelPos = WheelPos - 85
+		return 0, WheelPos * 3, 255 - WheelPos * 3
+	else
+		WheelPos = WheelPos - 170
+		return WheelPos * 3, 255 - WheelPos * 3, 0
+	end
+end
+
+
 if arg[1] == "replay" then
 	print("Start replay:", arg[2])
 	replay_dump = load_dump(arg[2])
@@ -72,7 +92,7 @@ if arg[1] == "replay" then
 	replay_len = #replay_dump
 else
 	replay = false
-	print("start dump:","log.dump")
+	print("start dump:", "log.dump")
 	file = io.open("log.dump", "w")
 	file:write(time_start_epoch .. "\n")
 end
@@ -83,7 +103,7 @@ while true do
 
 	timer_led = timer_led + dt
 	if timer_led > (1 / LED_FRAMERATE) then
-		send_led(counter/100)
+		send_led(counter)
 		counter = counter + 1
 		timer_led = 0
 	end
@@ -104,7 +124,7 @@ while true do
 			break
 		end
 	else
-		local data, ip, port = udp:receivefrom() -- receive from led, adc and super collider
+		local data, ip, port = udp:receivefrom() -- receive data from led, adc or super collider
 		if data then
 			-- print("Received: ", ip, port, #data)
 			local addr = osc.get_addr_from_data(data)
